5abfa8afb042857d5d13c18c0a6e58a7d689c5ab
zhangjialegh <zjl1195185765@outlook.com>
2019-06-14 11:59:01 +0800

    init

---
+++ entry.js
@@ -0,0 +1,36 @@
+const Koa = require('koa');
+const path = require('path');
+const app = new Koa();
+
+const {uploadFile} = require('./upload.js');
+app.use(async ctx => {
+  if(ctx.url === '/' && ctx.method === 'GET') {
+    let html = `
+      <h1>koa2 upload demo</h1>
+      <form method="POST" action="/upload.json" enctype="multipart/form-data">
+       <p>file upload</p>
+       <span>picName:</span><input name="picName" type="text"/><br/>
+       <input name="file" type="file"/><br/><br/>
+       <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+  } else if (ctx.url === '/upload.json' && ctx.method === 'POST') {
+    let result = {success: false}
+    let serverFilePath = path.join(__dirname,'upload-files') 
+
+    // 上传文件事件
+    result = await uploadFile(ctx, {
+      fileType: 'album',
+      path: serverFilePath
+    })
+
+    ctx.body = result
+  } else {
+    ctx.body = '<h1>404 !!!</h1>'
+  }
+})
+
+app.listen(3000,()=>{
+  console.log(`[demo] upload-simple is starting at post 3000`)
+})
---
+++ index.js
@@ -0,0 +1,47 @@
+const Koa = require('koa');
+const app = new Koa();
+const fs = require('fs');
+
+function render(page) {
+  return new Promise((resolve,reject)=>{
+    let viewUrl = `./view/${page}`;
+    fs.readFile(viewUrl,'binary',(err,data)=>{
+      if(err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+async function route(url) {
+  let view = '404.html'
+  console.log(url,'gg url')
+  switch (url) {
+    case '/':
+      view = 'index.html'
+      break;
+    case '/404':
+      view = '404.html'
+      break;
+    case '/todo':
+      view = 'todo.html'
+      break;
+  
+    default:
+      break;
+  }
+  let html = await render(view)
+  return html
+}
+
+app.use(async ctx => {
+  let url = ctx.request.url
+  let html = await route(url)
+  ctx.body = html
+})
+
+app.listen(3000,() => {
+  console.log(`At port 3000`)
+});
