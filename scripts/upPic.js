fce4955e5b134595414a2a2c1e1aed3d2cb7f916
zhangjiale <zhangjiale@btzh.cn>
2019-07-18 20:45:11 +0800

    fix

--- scripts/upPic.js
+++ scripts/upPic.js
@@ -23,7 +23,7 @@ app.use(async ctx => {
     await ctx.render('index',{
       title
     })
-  } else if(ctx.url.includes('api/picture/upload.json') && ctx.method === 'POST') {
+  } else if(ctx.url.includes('upload/pic') && ctx.method === 'POST') {
     let result = {success: false}
     let serverFilePath = path.join(__dirname,'../static/image')
 
